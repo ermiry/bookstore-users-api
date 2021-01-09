c6b17853c95880395540376ee0ab6ab002194e54
ermiry <erick.salas@ermiry.com>
2021-01-09 14:56:18 -0600

    Refactored controllers organization

--- src/app/url_mappings.go
+++ src/app/url_mappings.go
@@ -1,9 +1,13 @@
 package app
 
 import (
-	"../controllers"
+	"../controllers/ping"
+	"../controllers/users"
 )
 
 func mapURLs() {
-	router.GET("/ping", controllers.Ping)
+	router.GET("/ping", ping.Ping)
+
+	router.POST("/users", users.CreateUser)
+	router.GET("/users/:user_id", users.GetUser)
 }
